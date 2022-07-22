738f7dd2a730e23747245064439047fbddb008e2
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2022-07-21 18:21:39 -0600

    Commit 2

--- src/main/java/com/mycompany/appangelperez/Main.java
+++ src/main/java/com/mycompany/appangelperez/Main.java
@@ -1,7 +1,8 @@
 package com.mycompany.appangelperez;
 
 public class Main {
+    private static Operaciones op = new Operaciones();
     public static void main(String[] args){
-        System.out.println("Hello World");
+        op.Enumerar();
     }
 }
---
+++ src/main/java/com/mycompany/appangelperez/Operaciones.java
@@ -0,0 +1,9 @@
+package com.mycompany.appangelperez;
+
+public class Operaciones {
+    public void Enumerar(){
+        for(int x=1;x<11;x++){
+            System.out.print(x+" ");
+        }
+    }
+}
