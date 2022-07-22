692aa0c2b7d937ad6916c3ff0cc3b64e6470adf2
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2022-07-21 18:26:26 -0600

    Commit 4

--- src/main/java/com/mycompany/appangelperez/Main.java
+++ src/main/java/com/mycompany/appangelperez/Main.java
@@ -3,6 +3,9 @@ package com.mycompany.appangelperez;
 public class Main {
     private static Operaciones op = new Operaciones();
     public static void main(String[] args){
+        comenzar();
+    }
+    public static void comenzar(){
         op.Enumerar();
     }
 }
